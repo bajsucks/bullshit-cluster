c4e46c689e126a7d3506f21c33e43e3560ff111a
bajsucks <12kenvudsssq@gmail.com>
2025-03-16 15:47:40 +0200

    cookie.

--- CookieWrapper.js
+++ CookieWrapper.js
@@ -1,26 +1,29 @@
-function setCookie(cname, cvalue, exdays)
-{
-    const d = new Date();
-    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
-    let expires = "expires="+d.toUTCString();
-    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+function createCookie(name,value,days) {
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 *1000));
+        var expires = "; expires=" + date.toGMTString();
+    } else {
+        var expires = "";
+    }
+    document.cookie = name + "=" + value + expires + "; path=/";
 }
 
-function getCookie(cname)
-{
-    let name = cname + "=";
-    let ca = document.cookie.split(';');
-    for(let i = 0; i < ca.length; i++)
-    {
-        let c = ca[i];
-        while (c.charAt(0) == ' ')
-        {
-        c = c.substring(1);
+function readCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0;i < ca.length;i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') {
+            c = c.substring(1,c.length);
         }
-        if (c.indexOf(name) == 0)
-        {
-        return c.substring(name.length, c.length);
+        if (c.indexOf(nameEQ) == 0) {
+            return c.substring(nameEQ.length,c.length);
         }
     }
-    return "";
+    return null;
+}
+
+function removeCookie(name) {
+    createCookie(name,"",-1);
 }
