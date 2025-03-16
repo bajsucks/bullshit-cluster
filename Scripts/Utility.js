4731e02e140fb11ab951b41a72d392cd7662beee
bajsucks <12kenvudsssq@gmail.com>
2025-03-16 21:22:08 +0200

    added todo list and hookl utility

--- Scripts/Utility.js
+++ Scripts/Utility.js
@@ -3,3 +3,16 @@ function lerp(a, b, alpha)
 {
     return a + alpha * (b - a)
 }
+
+function Hook(url, text)
+{
+    var text = {content: text}
+
+    fetch(url, 
+    {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(text),
+    }
+    )
+}
